eabae1b6702615634deb7662b5afd0644415f759
Ashwini Soni <ashwsoni@deloitte.ca>
2020-10-11 00:11:35 -0400

    output changes 2

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "ip_address" {
-  value = google_compute_instance.docker.1.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.docker.0.network_interface.0.access_config.0.nat_ip
 }
 
